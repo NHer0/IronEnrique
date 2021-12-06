eefe88b4f7890ff39df05ee3950506b021ec5444
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-12-06 10:56:28 +0100

    Update Performance.py

--- Modules/Performance.py
+++ Modules/Performance.py
@@ -2,35 +2,48 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import r2_score, mean_absolute_error as mae, mean_squared_error as mse
+
+
+def r2_adjusted(x, y, y_pred, r2=None):
+
+    if r2 is not None:
+
+        r2 = r2_score(y, y_pred)
+
+    else:
+
+        r2_adj = 1 - (1 - r2) * (len(y) - 1) / (len(y) - x.shape[1] - 1)
+
+    return R2_adj
+
+def perf_regression(y_train, y_test, y_pred_train, y_pred_test, x_train, x_test, plot=False):
+
+    performance = pd.DataFrame({'error_metric': ['mae', 'mse', 'rmse', 'r2', 'r2_adj'],
+                                'train': [mae(y_train, y_pred_train),
+                                          mse(y_train, y_pred_train),
+                                          mse(y_train, y_pred_train, squared=False),
+                                          r2_score(y_train, y_pred_train),
+                                          r2_adjusted(x_train, y_train, y_pred_train)],
+                                'test':  [mae(y_test, y_pred_test),
+                                          mse(y_test, y_pred_test),
+                                          mse(y_test, y_pred_test, squared=False),
+                                          r2_score(y_test, y_pred_test),
+                                          r2_adjusted(x_test, y_test, y_pred_test)]})
+    performance = performance.set_index("error_metric")
 
 
 def perf_classification(y_train, y_test, y_pred_train, y_pred_test, pos_label=1, plot=False):
 
-    performance = pd.DataFrame({'Error_metric': ['Accuracy', 'Precision', 'Recall'],
-                                    'Train': [accuracy_score(y_train, y_pred_train),
-                                              precision_score(y_train, y_pred_train, pos_label=pos_label),
-                                              recall_score(y_train, y_pred_train, pos_label=pos_label)],
-                                    'Test': [accuracy_score(y_test, y_pred_test),
-                                             precision_score(y_test, y_pred_test, pos_label=pos_label),
-                                             recall_score(y_test, y_pred_test, pos_label=pos_label)]})
-    performance = performance.set_index("Error_metric")
+    performance = pd.DataFrame({'error_metric': ['accuracy', 'precision', 'recall'],
+                                'train': [accuracy_score(y_train, y_pred_train),
+                                          precision_score(y_train, y_pred_train, pos_label=pos_label),
+                                          recall_score(y_train, y_pred_train, pos_label=pos_label)],
+                                'test':  [accuracy_score(y_test, y_pred_test),
+                                          precision_score(y_test, y_pred_test, pos_label=pos_label),
+                                          recall_score(y_test, y_pred_test, pos_label=pos_label)]})
 
-    cm_train = confusion_matrix(y_train, y_pred_train)
-    cm_test = confusion_matrix(y_test, y_pred_test)
+    performance = performance.set_index("error_metric")
 
-    if plot:
+    return performance
 
-        print("Confusion matrix for the train set")
-        print("-----------------")
-
-        ConfusionMatrixDisplay.from_predictions(y_train, y_pred_train)
-        plt.show()
-
-        print("Confusion matrix for the test set")
-        print("-----------------")
-
-        ConfusionMatrixDisplay.from_predictions(y_test, y_pred_test)
-        plt.show()
-
-    return {"performance": performance, "train": {"cm": cm_train}, "test": {"cm": cm_test}}
